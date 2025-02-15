0bfa5183799ec261ba90c682f172c567b07fbfb7
Shyamsunder Tard <iamshyamsundertard@gmail.com>
2025-02-15 13:49:48 +0530

    Added Message and ChatSession collections, and updated email verification template and using Nodemailer

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,20 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+    email: {
+      config: {
+        provider: "nodemailer",
+        providerOptions: {
+          host: env("SMTP_HOST", "smtp.example.com"),
+          port: env("SMTP_PORT", 587),
+          auth: {
+            user: env("SMTP_USER", "your-email@example.com"),
+            pass: env("SMTP_PASS", "your-email-password"),
+          },
+        },
+        settings: {
+          defaultFrom: "noreply@example.com",
+          defaultReplyTo: "noreply@example.com",
+        },
+      },
+    },
+  });
+  
