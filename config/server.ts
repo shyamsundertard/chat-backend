5417c3663849420f7b0be9c398035aa15553898a
Shyamsunder Tard <iamshyamsundertard@gmail.com>
2025-02-18 15:20:09 +0530

    Added PUBLIC_URL for redirection for verification mail

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
+  url: env('PUBLIC_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
